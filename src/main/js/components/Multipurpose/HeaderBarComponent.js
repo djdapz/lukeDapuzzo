d2a85da9139f84125f5a693e19e2f561bc20a41e
djdapz <djdapz@aol.com>
2017-09-19 12:50:37 -0500

    added shaddow on menubar and removed header from homepage, kinda want to revert

--- src/main/js/components/Multipurpose/HeaderBarComponent.js
+++ src/main/js/components/Multipurpose/HeaderBarComponent.js
@@ -4,22 +4,34 @@ import React, {Component} from 'react';
 import style from "../../../style/components/header.sass";
 import {connect} from "react-redux";
 import {Link} from "react-router-dom";
+import routes from "../../constants/routes"
 
 
 
 class HeaderBar extends Component{
 
+
+
     render() {
         return (
             <div className="header-bar">
+                {this.renderHomeLink()}
+            </div>
+
+        )
+    }
+
+    renderHomeLink() {
+        if(this.props.route.href !== routes.HOME.href){
+            return(
+
                 <Link  to="/">
                     <p >
                         Luke D'Apuzzo
                     </p>
                 </Link>
-            </div>
-
-        )
+            )
+        }
     }
 }
 
