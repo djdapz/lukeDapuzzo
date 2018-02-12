1880289014c26d9509a8ab80a11c7eb82bc1fdc7
djdapz <djdapz@aol.com>
2018-02-12 00:37:05 -0600

    Brought in Flexbox to fix menubar subtitle wrapping issue
    + Upgraded Bootstrap
    
    + Refactored CSS
    
    [#155114351]

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,8 @@ import {Provider} from 'react-redux';
 import {applyMiddleware, createStore} from 'redux';
 import ReduxPromise from 'redux-promise';
 
+import style from "../style/app.sass"
+
 import App from './components/app';
 import reducers from './reducers/index';
 import {BrowserRouter} from "react-router-dom";
