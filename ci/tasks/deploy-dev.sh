f57190750fad4d2329dd5a80ef2130936a8b3429
djdapz <djdapz@aol.com>
2018-04-06 12:23:28 -0500

    Try to get gradle to cache.

--- ci/tasks/deploy-dev.sh
+++ ci/tasks/deploy-dev.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 wget -q -O - https://packages.cloudfoundry.org/debian/cli.cloudfoundry.org.key | sudo apt-key add -
 echo "deb https://packages.cloudfoundry.org/debian stable main" | sudo tee /etc/apt/sources.list.d/cloudfoundry-cli.list
