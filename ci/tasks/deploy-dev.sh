845f2fdac4b745ead06df1d5e662af1add4cca32
djdapz <djdapz@aol.com>
2018-04-06 00:34:09 -0500

    add dowload for cf cli in deploy-dev

--- ci/tasks/deploy-dev.sh
+++ ci/tasks/deploy-dev.sh
@@ -1,21 +1,26 @@
 #!/usr/bin/env sh
 
+wget -q -O - https://packages.cloudfoundry.org/debian/cli.cloudfoundry.org.key | sudo apt-key add -
+echo "deb https://packages.cloudfoundry.org/debian stable main" | sudo tee /etc/apt/sources.list.d/cloudfoundry-cli.list
+sudo apt-get update
+sudo apt-get install cf-cli
+
 #################
 ## push server ##
 #################
 
-pushd repo
-    cf login api.run.pivotal.io -s development -u $PCF_USERNAME -p $PCF_PASSWORD
-    ./gradlew clean test bootRepackage
-    cf push -p build/libs/lukeDapuzzo-0.0.1-SNAPSHOT.jar
+cd repo
+
+cf login api.run.pivotal.io -s development -u $PCF_USERNAME -p $PCF_PASSWORD
+./gradlew clean test bootRepackage
+cf push -p build/libs/lukeDapuzzo-0.0.1-SNAPSHOT.jar
 
 #################
 ## push client ##
 #################
 
-    pushd client
-        cf login api.run.pivotal.io -s development -u $PCF_USERNAME -p $PCF_PASSWORD
-        npm run build
-        cf push -f ../ci/manifests/dev/client-manifest.yml
-    popd
-popd
+cd client
+
+cf login api.run.pivotal.io -s development -u $PCF_USERNAME -p $PCF_PASSWORD
+npm run build
+cf push -f ../ci/manifests/dev/client-manifest.yml
