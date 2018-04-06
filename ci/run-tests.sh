72ce1a21db0b9607e8997173f7f92ea2b4fccbbe
djdapz <djdapz@aol.com>
2018-04-05 23:46:17 -0500

    moving pipeline into /ci directory

---
+++ ci/run-tests.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "here"
+
+./gradlew clean test
---
+++ ci/tasks/deploy-dev.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+#################
+## push server ##
+#################
+
+pushd repo
+    cf login api.run.pivotal.io -s development -u $PCF_USERNAME -p $PCF_PASSWORD
+    ./gradlew clean test bootRepackage
+    cf push -p build/libs/lukeDapuzzo-0.0.1-SNAPSHOT.jar
+
+#################
+## push client ##
+#################
+
+    pushd client
+        cf login api.run.pivotal.io -s development -u $PCF_USERNAME -p $PCF_PASSWORD
+        npm run build
+        cf push -f ../ci/manifests/dev/client-manifest.yml
+    popd
+popd
