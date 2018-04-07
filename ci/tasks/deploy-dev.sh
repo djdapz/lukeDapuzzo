cd3ff61fba46a27361c844f2df4bfaa89d53eae8
djdapz <djdapz@aol.com>
2018-04-07 17:11:06 -0500

    remove sudo and test

--- ci/tasks/deploy-dev.sh
+++ ci/tasks/deploy-dev.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-wget -q -O - https://packages.cloudfoundry.org/debian/cli.cloudfoundry.org.key | sudo apt-key add -
-echo "deb https://packages.cloudfoundry.org/debian stable main" | sudo tee /etc/apt/sources.list.d/cloudfoundry-cli.list
+ls -lsa /usr/bin
+
+wget -q -O - https://packages.cloudfoundry.org/debian/cli.cloudfoundry.org.key | apt-key add -
+echo "deb https://packages.cloudfoundry.org/debian stable main" | tee /etc/apt/sources.list.d/cloudfoundry-cli.list
 apt-get update
 apt-get install cf-cli
 
@@ -12,7 +14,7 @@ apt-get install cf-cli
 cd repo
 
 cf login api.run.pivotal.io -s development -u $PCF_USERNAME -p $PCF_PASSWORD
-./gradlew clean test bootRepackage
+./gradlew clean bootRepackage
 cf push -p build/libs/lukeDapuzzo-0.0.1-SNAPSHOT.jar
 
 #################
