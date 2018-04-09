0f3e068d4793a2c0aff8c07d8f2dfbb6b4f3a685
djdapz <djdapz@aol.com>
2018-04-09 18:58:25 -0500

    simplify common and remove sudo from cf task

--- ci/tasks/common.sh
+++ ci/tasks/common.sh
@@ -4,4 +4,6 @@ GRADLE_HOME="${HOME}/.gradle"
 ROOT_FOLDER=$(pwd)
 GRADLE_CACHE="${ROOT_FOLDER}/gradle"
 
-test -d $GRADLE_CACHE ! -d $GRADLE_HOME  && ln -s $GRADLE_CACHE $GRADLE_HOME
+if [ "$GRADLE_CACHE" == "$GRADLE_HOME" ]
+    then ln -s $GRADLE_CACHE $GRADLE_HOME
+fi
--- ci/tasks/install-cf-cli.sh
+++ ci/tasks/install-cf-cli.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-wget -q -O - https://packages.cloudfoundry.org/debian/cli.cloudfoundry.org.key | sudo apt-key add -
-sudo apt-get update
-sudo apt-get install cf-cli
+wget -q -O - https://packages.cloudfoundry.org/debian/cli.cloudfoundry.org.key | apt-key add -
+apt-get update
+apt-get install cf-cli
