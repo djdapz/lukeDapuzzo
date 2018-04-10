2f463dcd3e92279e24b9113f188733a0a7c9b419
djdapz <djdapz@aol.com>
2018-04-09 19:09:35 -0500

    more logging

--- ci/tasks/common.sh
+++ ci/tasks/common.sh
@@ -7,3 +7,8 @@ GRADLE_CACHE="${ROOT_FOLDER}/gradle"
 if [ "$GRADLE_CACHE" == "$GRADLE_HOME" ]
     then ln -s $GRADLE_CACHE $GRADLE_HOME
 fi
+
+echo GRADLE_CACHE
+echo $GRADLE_CACHE
+echo GRADLE_HOME
+echo $GRADLE_HOME
