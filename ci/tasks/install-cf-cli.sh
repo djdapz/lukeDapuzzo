c483b9c51dbc9e1557bc6428e1d653798861eaea
djdapz <djdapz@aol.com>
2018-04-09 20:06:21 -0500

    tried to add && to install cf cli

--- ci/tasks/install-cf-cli.sh
+++ ci/tasks/install-cf-cli.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
 
-wget -O cf-cli https://cli.run.pivotal.io/stable?release=debian64&source=github
-dpkg -i cf-cli
+wget  https://cli.run.pivotal.io/stable?release=debian64&source=github && dpkg -i cf-cli
