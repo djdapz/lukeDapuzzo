cce235c1c837a22cba0530a1fc92487bb98e9900
djdapz <djdapz@aol.com>
2018-04-09 19:27:20 -0500

    adding cf packages as soruce

--- ci/tasks/common.sh
+++ ci/tasks/common.sh
@@ -6,18 +6,8 @@ GRADLE_CACHE="${ROOT_FOLDER}/gradle"
 
 if [ "$GRADLE_CACHE" != "$GRADLE_HOME" ];
     then
-        echo "HERE"
-        echo "HERE"
-        echo "HERE"
         ln -s $GRADLE_CACHE $GRADLE_HOME
-     else
-        echo "SAD"
-        echo "SAD"
-        echo "SAD"
-        echo "SAD"
 fi
 
-echo GRADLE_CACHE
-echo $GRADLE_CACHE
-echo GRADLE_HOME
-echo $GRADLE_HOME
+cd HOME
+ls -lsa
--- ci/tasks/install-cf-cli.sh
+++ ci/tasks/install-cf-cli.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 wget -q -O - https://packages.cloudfoundry.org/debian/cli.cloudfoundry.org.key | apt-key add -
+echo "deb https://packages.cloudfoundry.org/debian stable main" | sudo tee /etc/apt/sources.list.d/cloudfoundry-cli.list
 apt-get update
 apt-get install cf-cli
