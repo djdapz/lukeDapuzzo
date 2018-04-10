c2341db64cbe75edcb643c88826b45dd16bc13d9
djdapz <djdapz@aol.com>
2018-04-09 19:40:10 -0500

    I THINK THIS WILL WORK - using wget on the pivotal url

--- ci/tasks/install-cf-cli.sh
+++ ci/tasks/install-cf-cli.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-wget -q -O - https://packages.cloudfoundry.org/debian/cli.cloudfoundry.org.key | apt-key add -
-echo "deb https://packages.cloudfoundry.org/debian stable main" | sudo tee /etc/apt/sources.list.d/cloudfoundry-cli.list
-apt-get update
-apt-get install cf-cli
+
+wget -O cf-cli.deb https://cli.run.pivotal.io/stable?release=debian64&source=github
+dpkg -i cf-cli.deb
