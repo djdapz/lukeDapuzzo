23ef2714ae26ea553fe814e6ec50c8392e81fed9
djdapz <djdapz@aol.com>
2018-04-12 12:43:29 -0500

    Trying to get dcind to copy repo and gradle cache.

--- ci/tasks/run-tests.sh
+++ ci/tasks/run-tests.sh
@@ -2,4 +2,10 @@
 
 echo "here"
 
-./gradlew clean tests
+export GRADLE_HOME="/gradle/.gradle"
+export GRADLE_USER_HOME="/gradle/.usergradle"
+export GRADLE_CACHE="/gradle/cache"
+
+pushd repo
+    ./gradlew clean tests
+popd
