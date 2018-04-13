c77e20eb926246e2e4fe98fecc9319ec3c57da15
djdapz <djdapz@aol.com>
2018-04-13 13:01:52 -0500

    export db env variables in test job

--- ci/tasks/run-tests.sh
+++ ci/tasks/run-tests.sh
@@ -8,6 +8,10 @@ export GRADLE_HOME="/gradle/.gradle"
 export GRADLE_USER_HOME="/gradle/.usergradle"
 export GRADLE_CACHE="/gradle/cache"
 
+export LUKE_DB_URL=jdbc:postgresql://luke-db:5432/luke-test
+export LUKE_DB_USERNAME=lukeuser
+export LUKE_DB_PASSWORD=lukepwd
+
 pushd repo
     ./gradlew clean test
 popd
