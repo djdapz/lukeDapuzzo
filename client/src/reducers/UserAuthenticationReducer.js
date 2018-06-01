38f1d9cf5c3549c951fcd6a3f2dc77ca9ec7bddd
djdapz <djdapz@aol.com>
2018-05-31 20:47:14 -0500

    re-enable security on the front end

--- client/src/reducers/UserAuthenticationReducer.js
+++ client/src/reducers/UserAuthenticationReducer.js
@@ -1,15 +1,15 @@
 import {AUTHORIZE_USER, FAIL_LOGIN} from "../actions/UserActions";
 
 const userAuthenticationReducer = (state = false, action) => {
-    // if(action.type === AUTHORIZE_USER){
-    //     return true;
-    // }
-    //
-    // if(action.type === FAIL_LOGIN){
-    //     return false;
-    // }
+    if (action.type === AUTHORIZE_USER) {
+        return true;
+    }
 
-    return true;
+    if (action.type === FAIL_LOGIN) {
+        return false;
+    }
+
+    return state;
 };
 
 export default userAuthenticationReducer
