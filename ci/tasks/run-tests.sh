3d48d7f45f5a5d65bfc86dddf57c893e1f4c1961
djdapz <djdapz@aol.com>
2018-06-01 12:55:59 -0500

    MORE

--- ci/tasks/run-tests.sh
+++ ci/tasks/run-tests.sh
@@ -7,16 +7,16 @@ export GRADLE_USER_HOME="/gradle/.usergradle"
 export GRADLE_CACHE="/gradle/caches"
 
 export LUKE_DB_URL=jdbc:postgresql://luke-db:5432/luke-test?user=lukeuser&password=lukepwd
-export LUKE_DB_USERNAME=lukeuser
+export  LUKE_DB_USERNAME=lukeuser
 export LUKE_DB_PASSWORD=lukepwd
 
 echo "about to run tests"
 echo "LUKE_DB_URL"
-echo LUKE_DB_URL
+echo $LUKE_DB_URL
 echo "LUKE_DB_USERNAME"
-echo LUKE_DB_USERNAME
+echo $LUKE_DB_USERNAME
 echo "LUKE_DB_PASSWORD"
-echo LUKE_DB_PASSWORD
+echo $LUKE_DB_PASSWORD
 
 pushd repo
     ./gradlew clean test
