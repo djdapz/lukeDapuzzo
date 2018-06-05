c8b63d60f31581df358ce480646e2bb5457c6921
djdapz <djdapz@aol.com>
2018-06-05 12:43:38 -0500

    make sure that concourse does not push node_modules to CF

--- deploy-prod.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-
-cf login api.run.pivotal.io -s production -u $PCF_USERNAME
-
-./gradlew clean  bootRepackage
-
-cf push -p build/libs/lukeDapuzzo-0.0.1-SNAPSHOT.jar -f manifest-prod.yml
--- deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-cf login api.run.pivotal.io -s development -u $PCF_USERNAME
-./gradlew clean test bootRepackage
-cf push -p build/libs/lukeDapuzzo-0.0.1-SNAPSHOT.jar
