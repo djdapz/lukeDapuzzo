6c26c696945a1d5adfa15aa2f27f4c8299e072c4
djdapz <djdapz@aol.com>
2018-06-13 23:16:30 -0500

    update build task to reflect change in frontend strucutre.

--- ci/tasks/build-and-upload.sh
+++ ci/tasks/build-and-upload.sh
@@ -9,7 +9,7 @@ export BUILD_VERSION=`cat version/number`
 pushd repo
     ./gradlew clean assemble -Pversion=$BUILD_VERSION
     /root/bin/aws s3 cp "build/libs/luke-dapuzzo-${BUILD_VERSION}.jar" "s3://luke-dapuzzo/app/luke-dapuzzo-${BUILD_VERSION}.jar"
-    pushd client
+    pushd frontend
         npm install
         npm run build
         /root/bin/aws s3 cp "./dist/bundle.js" "s3://luke-dapuzzo/app/bundle-${BUILD_VERSION}.js"
