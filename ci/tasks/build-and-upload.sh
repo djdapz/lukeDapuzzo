d2378bd71a462820372efa59a8b5eee026752bba
djdapz <djdapz@aol.com>
2018-06-15 23:34:21 -0500

    Add some logging to build and upload step

--- ci/tasks/build-and-upload.sh
+++ ci/tasks/build-and-upload.sh
@@ -6,13 +6,16 @@ source repo/ci/tasks/common.sh
 
 export BUILD_VERSION=`cat version/number`
 
-pushd repo
-    ./gradlew clean assemble -Pversion=$BUILD_VERSION
-    /root/bin/aws s3 cp "build/libs/luke-dapuzzo-${BUILD_VERSION}.jar" "s3://luke-dapuzzo/app/luke-dapuzzo-${BUILD_VERSION}.jar"
-    pushd frontend
-        ls -lsa
-        npm install
-        npm run build
-        /root/bin/aws s3 cp "./dist/bundle.js" "s3://luke-dapuzzo/app/bundle-${BUILD_VERSION}.js"
-    popd
-popd
+cd repo
+./gradlew clean assemble -Pversion=$BUILD_VERSION
+echo " >>>> Uploading jar to aws"
+/root/bin/aws s3 cp "build/libs/luke-dapuzzo-${BUILD_VERSION}.jar" "s3://luke-dapuzzo/app/luke-dapuzzo-${BUILD_VERSION}.jar"
+echo " <<<< Uploaded jar to aws"
+
+cd frontend
+npm install
+npm run build
+
+echo " >>>> Uploading bundle.js to aws"
+/root/bin/aws s3 cp "./dist/bundle.js" "s3://luke-dapuzzo/app/bundle-${BUILD_VERSION}.js"
+echo " <<<< Uploaded bundle.js to aws"
