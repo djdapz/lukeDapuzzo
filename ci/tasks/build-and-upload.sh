0c2d485ecfcc0263e9228ed467869e3664a38b87
djdapz <djdapz@aol.com>
2018-06-15 23:47:59 -0500

    Adjust shell script to hopefully pass when it should...

--- ci/tasks/build-and-upload.sh
+++ ci/tasks/build-and-upload.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
-set -e
+set -ex
 
 source repo/ci/tasks/common.sh
 
 export BUILD_VERSION=`cat version/number`
 
-cd repo
-./gradlew clean assemble -Pversion=$BUILD_VERSION
-echo " >>>> Uploading jar to aws"
-/root/bin/aws s3 cp "build/libs/luke-dapuzzo-${BUILD_VERSION}.jar" "s3://luke-dapuzzo/app/luke-dapuzzo-${BUILD_VERSION}.jar"
-echo " <<<< Uploaded jar to aws"
+pushd repo
+    ./gradlew clean assemble -Pversion=$BUILD_VERSION
+    echo " >>>> Uploading jar to aws"
+    /root/bin/aws s3 cp "build/libs/luke-dapuzzo-${BUILD_VERSION}.jar" "s3://luke-dapuzzo/app/luke-dapuzzo-${BUILD_VERSION}.jar"
+    echo " <<<< Uploaded jar to aws"
 
-cd frontend
-npm install
-npm run build
+    pushd frontend
+        npm install
+        npm run build
 
-echo " >>>> Uploading bundle.js to aws"
-/root/bin/aws s3 cp "./dist/bundle.js" "s3://luke-dapuzzo/app/bundle-${BUILD_VERSION}.js"
-echo " <<<< Uploaded bundle.js to aws"
+        echo " >>>> Uploading bundle.js to aws"
+        /root/bin/aws s3 cp "./dist/bundle.js" "s3://luke-dapuzzo/app/bundle-${BUILD_VERSION}.js"
+        echo " <<<< Uploaded bundle.js to aws"
+    popd
+popd
