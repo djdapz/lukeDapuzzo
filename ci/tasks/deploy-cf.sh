eccb952ea92ff2c5f77c56b41a46f2f63025a851
djdapz <djdapz@aol.com>
2018-06-16 00:06:00 -0500

    Fix file extension in deploy cf task

--- ci/tasks/deploy-cf.sh
+++ ci/tasks/deploy-cf.sh
@@ -14,7 +14,7 @@ cf login -a api.run.pivotal.io -s $PCF_SPACE -u $PCF_USERNAME -p $PCF_PASSWORD
 
 
 pushd repo
-    /root/bin/aws s3 cp "s3://luke-dapuzzo/app/luke-dapuzzo-${BUILD_VERSION}.js"  ./luke.jar
+    /root/bin/aws s3 cp "s3://luke-dapuzzo/app/luke-dapuzzo-${BUILD_VERSION}.jar"  ./luke.jar
     cf push -p "./luke.jar" -f "./ci/manifests/$PCF_SPACE/server-manifest.yml"
 popd
 
