91feec946f99468ed8513b2b78b2cacbad75695a
djdapz <djdapz@aol.com>
2018-06-25 18:24:40 -0500

    More logging in server.js and don't boot until the file is saved

--- frontend-server/server.js
+++ frontend-server/server.js
@@ -8,22 +8,22 @@ const fs = require('fs');
 const port = process.env.PORT || 3000;
 const buildDir = __dirname + "/dist";
 
+console.log("About to create config file");
 
-fs.writeFile(buildDir + "/config.js", "var env =" +JSON.stringify(config) , function(err) {
-    if(err) {
+fs.writeFile(buildDir + "/config.js", "var env =" + JSON.stringify(config), function (err) {
+    if (err) {
         return console.log(err);
     }
 
     console.log("The file was saved!");
-});
 
+    app.listen(port);
+    console.log("Listening on port: " + port);
+
+});
 
 app.use(express.static(buildDir));
 
-app.get('*', function (request, response){
+app.get('*', function (request, response) {
     response.sendFile(path.resolve(buildDir, 'index.html'))
 });
-
-app.listen(port);
-
-console.log("Listening on port: " + port);
