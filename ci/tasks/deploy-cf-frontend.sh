9f57c3d6a3a496bd578e7768d41e5622fdd769a5
djdapz <djdapz@aol.com>
2018-10-18 18:49:05 -0500

    Fix copy command from aws

--- ci/tasks/deploy-cf-frontend.sh
+++ ci/tasks/deploy-cf-frontend.sh
@@ -6,7 +6,7 @@ export BUILD_VERSION=`cat version/number`
 
 cf login -a api.run.pivotal.io -s $PCF_SPACE -u $PCF_USERNAME -p $PCF_PASSWORD
 
-/root/bin/aws s3 cp "./frontend-server.zip"
+/root/bin/aws s3 cp "./frontend-server.zip" ./frontend-server-build.zip
 
 /root/bin/aws s3 cp  "s3://luke-dapuzzo/app/frontend-server-{BUILD_VERSION}.zip"  ./frontend-server-build.zip
 cf push -f "../ci/manifests/$PCF_SPACE/client-manifest.yml" -p ./frontend-server-build.zip
