66f3e4a96cfcec85ab1c7a3c11d3765d13935f71
djdapz <djdapz@aol.com>
2018-10-18 19:08:28 -0500

    fix

--- ci/tasks/run-tests.sh
+++ ci/tasks/run-tests.sh
@@ -6,12 +6,12 @@ export GRADLE_HOME="/gradle/.gradle"
 export GRADLE_USER_HOME="/gradle/.usergradle"
 export GRADLE_CACHE="/gradle/caches"
 
-export GRADLE_HOME
-export $GRADLE_HOME
-export GRADLE_USER_HOME
-export $GRADLE_USER_HOME
-export GRADLE_CACHE
-export $GRADLE_CACHE
+echo GRADLE_HOME
+echo $GRADLE_HOME
+echo GRADLE_USER_HOME
+echo $GRADLE_USER_HOME
+echo GRADLE_CACHE
+echo $GRADLE_CACHE
 
 
 export LUKE_DB_URL="jdbc:postgresql://luke-db:5432/luke-test?user=lukeuser&password=lukepwd"
