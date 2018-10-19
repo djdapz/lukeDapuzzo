119ac5128006565dec019aeb3961680294f6a9a9
djdapz <djdapz@aol.com>
2018-10-18 21:38:28 -0500

    Update bio for luke.

--- frontend/src/components/BioPage/BioPageComponent.js
+++ frontend/src/components/BioPage/BioPageComponent.js
@@ -6,40 +6,17 @@ class BioPage extends Component {
         return (<div id="bio-page">
                 <img src={Image}/>
 
-                <div  className="main-content">
+                <div className="main-content">
                     <p>
-                        I'm Luke D'Apuzzo and I have been a musician my whole life. I originally started playing the
-                        drums
-                        at
-                        the age of 6 then after a few years moved to guitar. After that I started singing and playing
-                        bass
-                        too and because fascinated in the idea of learning to record my own music. Currently I write my
-                        own
-                        music and combine a modern rock sound with electronic elements and influences from a large
-                        variety
-                        of genres to create a new sound of my own. Over the past few years I have been recording all of
-                        my
-                        own songs on Ableton live playing the guitar, drums, bass, piano and singing my own tracks then
-                        mixing them together.
+                        Luke Dapuzzo is a solo independent recording artist from Boulder Colorado. His unique sound
+                        blends together a mix of influences from rockabilly to metalcore.
                     </p>
 
                     <p>
-                        Currently I've been trying to make a name for myself and am constantly writing new music. I
-                        always
-                        wanted to be a musician for a profession and want my music to be heard and inspire the people
-                        who
-                        listen to it. I am also a performer and have recently done a good amount of acoustic shows and
-                        worked in a good amount of bands anywhere from drummer to lead singer and guitarist.
-                    </p>
-
-                    <p>
-                        Influences: David Bowie, My Chemical Romance, The Beatles, Green Day, Pierce the Veil, Frank
-                        Iero,
-                        Bring me the Horizon and many more…
+                        All music released by this artist was recorded and mixed by Luke himself.
                     </p>
                 </div>
             </div>
-
         )
     }
 }
