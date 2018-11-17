d5b4053957cd7a0576b2ee34912e876895e36486
djdapz <djdapz@aol.com>
2018-11-16 21:33:22 -0600

    add paper for luke
    s bio

--- frontend/src/components/BioPage/BioPageComponent.js
+++ frontend/src/components/BioPage/BioPageComponent.js
@@ -1,21 +1,25 @@
 import React, {Component} from 'react';
 import Image from './outside_picture.jpg'
+import Paper from "@material-ui/core/Paper/Paper";
 
 class BioPage extends Component {
     render() {
-        return <div id="bio-page">
-            <img src={Image}
-                 alt={"Luke preforming guitar, drums, and singing"}/>
+        return <div id="bio-page"
+                    className="main-content">
+            <div id={'bio-content'}>
+                <img src={Image}
+                     alt={"Luke preforming guitar, drums, and singing"}/>
 
-            <div className="main-content">
-                <p>
-                    Luke Dapuzzo is a solo independent recording artist and songwriter from Boulder, Colorado. His
-                    unique sound blends together a variety of genres from rockabilly to metalcore.
-                </p>
+                <Paper className="bio">
+                    <p>
+                        Luke Dapuzzo is a solo independent recording artist and songwriter from Boulder, Colorado. His
+                        unique sound blends together a variety of genres from rockabilly to metalcore.
+                    </p>
 
-                <p>
-                    All music released by this artist was performed, recorded, and mixed by Luke himself.
-                </p>
+                    <p>
+                        All music released by this artist was performed, recorded, and mixed by Luke himself.
+                    </p>
+                </Paper>
             </div>
         </div>
     }
