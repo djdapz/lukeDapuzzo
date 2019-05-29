a071674beeb9ea699b4a0914dcd7e16808cc2ca5
djdapz <djdapz@aol.com>
2019-05-28 18:58:28 -0600

    set db url with username and password in it for test

--- ci/tasks/run-tests.sh
+++ ci/tasks/run-tests.sh
@@ -5,10 +5,11 @@ cd repo
 
 pwd
 
-env
 
-export LUKE_DB_URL=jdbc:postgresql://luke-db:5432/luke-test
+export LUKE_DB_URL="jdbc:postgresql://luke-db:5432/luke-test?user=lukeuser&password=lukepwd"
 export LUKE_DB_PASSWORD=lukeuser
 export LUKE_DB_USERNAME=lukepwd
+
+env
 
 ./gradlew clean test
