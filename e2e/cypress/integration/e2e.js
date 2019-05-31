bb7536c8e8d9c29725450896a1fd1a60c928a10d
djdapz <djdapz@aol.com>
2019-05-31 00:25:35 -0700

    remove need for https vs http in config

--- e2e/cypress/integration/e2e.js
+++ e2e/cypress/integration/e2e.js
@@ -1,6 +1,6 @@
 describe("Luke's Website", () => {
     beforeEach(function () {
-        cy.visit("https://https://luke-dapuzzo-client.cfapps.io/")
+        cy.visit("http://luke-dapuzzo-client.cfapps.io/")
     });
     it('should show the subheader', function () {
         cy.contains("A solo songwriter and musician from Boulder, Colorado")
