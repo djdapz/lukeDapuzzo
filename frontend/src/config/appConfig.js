a53b49e91b414ec000a1c35b44251be55914cbfe
djdapz <djdapz@aol.com>
2019-05-31 00:31:33 -0700

    hopefully avoid re-downloading openjdk

--- frontend/src/config/appConfig.js
+++ frontend/src/config/appConfig.js
@@ -1,44 +1,42 @@
-const LOCAL = "LOCAL";
-const DEV = "DEV";
-const PROD = "PROD";
-const TEST = "TEST";
-const CYPRESS = "CYPRESS";
-
+const LOCAL = "LOCAL"
+const DEV = "DEV"
+const PROD = "PROD"
+const TEST = "TEST"
+const CYPRESS = "CYPRESS"
 
 const environment = function () {
-    switch (window.location.origin) {
-        case ("http://localhost:3000"):
-            return LOCAL;
-        case ("http://localhost:3210"):
-            return CYPRESS;
-        case("https://luke-dapuzzo-client.cfapps.io"):
-            return DEV;
-        case("https://luke-dapuzzo-client-static.cfapps.io"):
-            return DEV;
-        case("https://lukedapuzzo.com"):
-            return PROD;
-        case("https://www.lukedapuzzo.com"):
-            return PROD;
-        case("https://luke-dapuzzo-client-prod.cfapps.io"):
-            return PROD;
-        case("https://luke-dapuzzo-dot-com-prod.cfapps.io"):
-            return PROD;
-        default:
-            return TEST
-    }
-}();
+  switch (window.location.host) {
+    case ("localhost:3000"):
+      return LOCAL
+    case ("localhost:3210"):
+      return CYPRESS
+    case("luke-dapuzzo-client.cfapps.io"):
+      return DEV
+    case("luke-dapuzzo-client-static.cfapps.io"):
+      return DEV
+    case("lukedapuzzo.com"):
+      return PROD
+    case("www.lukedapuzzo.com"):
+      return PROD
+    case("luke-dapuzzo-client-prod.cfapps.io"):
+      return PROD
+    case("luke-dapuzzo-dot-com-prod.cfapps.io"):
+      return PROD
+    default:
+      return TEST
+  }
+}()
 
-
-function loadEnv(options) {
-    return options[environment] || options[LOCAL]
+function loadEnv (options) {
+  return options[environment] || options[LOCAL]
 }
 
 let LUKE_API = loadEnv({
-    LOCAL: "http://localhost:8081",
-    DEV: "https://luke-dapuzzo-api-dev.cfapps.io",
-    PROD: "https://luke-dapuzzo-api.cfapps.io",
-    TEST: "www.garbage-url.com",
-    CYPRESS: "http://localhost:3210"
-});
+  LOCAL: "http://localhost:8081",
+  DEV: "https://luke-dapuzzo-api-dev.cfapps.io",
+  PROD: "https://luke-dapuzzo-api.cfapps.io",
+  TEST: "www.garbage-url.com",
+  CYPRESS: "http://localhost:3210"
+})
 
-export {LUKE_API}
+export { LUKE_API }
